8f36cc06fe809b5d0f5b53442e1a2f099409eebc
Shershneva Kseniya <megacoolksuha@mail.ru>
2025-08-22 10:49:58 +0300

    chore: add eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals, { browser } from 'globals';
+import reactHooks, { configs, rules } from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint, { config } from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import { version } from 'typescript';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -11,13 +12,62 @@ export default tseslint.config([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      ...tseslint.configs.strict,
+      eslintPluginPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react-compiler/react-compiler': 'error',
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'padding-line-between-statements': [
+        2,
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: ['return', 'break'],
+        },
+        {
+          blankLine: 'always',
+          prev: ['const', 'let'],
+          next: '*',
+        },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let'],
+          next: ['const', 'let'],
+        },
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: 'if',
+        },
+        {
+          blankLine: 'always',
+          prev: 'if',
+          next: '*',
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-])
+]);
